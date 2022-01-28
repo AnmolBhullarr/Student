23065a1e20bee4a2029e9db38988be3d37041188
AnmolBhullarr <98172759+AnmolBhullarr@users.noreply.github.com>
2022-01-28 09:47:19 -0500

    Update Student1.java

--- src/student/Student1.java
+++ src/student/Student1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Author: Anmol Bhullar
+ Date: Jan 28th 2022
  */
 package student;
 
